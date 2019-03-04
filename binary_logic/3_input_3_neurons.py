55b0fb0b4e46d8c39ceb6d11eeaa159b4316cfd8
wgruen <wolf128git@gmail.com>
2019-03-04 18:54:02 -0500

    one day of work  and this machine works

--- binary_logic/3_input_3_neurons.py
+++ binary_logic/3_input_3_neurons.py
@@ -30,6 +30,7 @@ class NeuralNetwork():
         for i in range(number_of_input_neurons):
             self.input_neurons.append(Neuron(0, 1))
             
+            
         for i in range(number_of_input_neurons):
             self.hidden_neurons.append(Neuron(1, number_of_inputs)) 
             
@@ -73,75 +74,82 @@ class NeuralNetwork():
                 expected_output = training_set_outputs[i]
                 #print("Expected output: ", expected_output, linesep)
             
-                # each input paramter will have its own neuron
-                number_of_inputs = len(array)
-                #print("Number of inputs: ", number_of_inputs)
-                for i1 in range(0, number_of_inputs):
-                    in_value = array[i1]
-                    #print("Input value: ", in_value)
-                
-                    self.input_neurons[i1].train_push_input(in_value)
-                
-                # provide the output of the previous layer to the next layer
-                # There is only one hidden layer so keep this simple
-                input_next_layer = []
-                input_next_layer.append(987654)
-                for i2 in range(0, len(self.input_neurons)):
-                    input_next_layer.extend(self.input_neurons[i2].normalized_neuron_value[0])
-#                    input_next_layer.extend(self.hidden_neurons[i2].normalized_neuron_value)
-                    
-                input_next_layer = input_next_layer[1:]
-                #print("Variables for the next hidden layer: ",  input_next_layer)
-                #break
-            
-                for i3 in range(0, len(self.hidden_neurons)):
-                    self.hidden_neurons[i3].train_push_input(input_next_layer)
-                #break
-                    
-                # provide the output of the previous layer to the next layer
-                # There is only one hidden layer so keep this simple
-                input_next_layer = []
-                input_next_layer.append(987654)
-                for i4 in range(0, len(self.hidden_neurons)):
-                    input_next_layer.extend(self.hidden_neurons[i4].normalized_neuron_value)
-                    
-                input_next_layer = input_next_layer[1:]
-                #print("Variables for the next output layer: ",  input_next_layer)
-                #break
-                
-                # simple for nw, there is only on output neuron
-                output_neuron_value = 0
-                for i5 in range(len(self.output_neurons)):
-                    error = self.output_neurons[i5].train_push_input(input_next_layer)
-                    
-            
+                nn_output = self.think(array)
                      
                 # Calculate the error (The difference between the desired output
                 # and the predicted output).
-                error = expected_output - output_neuron_value
+                error = expected_output - nn_output
                 error_total = 0.5 * pow(error, 2)
                 
-                # this specific machine uses one ouptu value only 
+                # this specific machine uses one ouput neuron 
                 # and therefore the backward pass is simpler
-                out_minus_target = -1 * ( expected_output - error_total )
+    
                 
                 # 1st we update the last layer, the output layer
+                need_for_next_layer = 0
                 for i6 in range(len(self.output_neurons)):
-                    error = self.output_neurons[i6].train_adjust_synaptic_weights(out_minus_target)
-                
+                    error = self.output_neurons[i6].train_adjust_synaptic_weights_for_output_neuron(expected_output)
+                    need_for_next_layer = self.output_neurons[i6].derived_normalized_vs_value * \
+                        self.output_neurons[i6].out_minus_target
+                    
                 # Next the hidden layer
+                
                 for i7 in range(len(self.hidden_neurons)):
-                    error = self.hidden_neurons[i7].train_adjust_synaptic_weights(out_minus_target)
+                    error = self.hidden_neurons[i7].train_adjust_synaptic_weights_for_hidden_neuron(need_for_next_layer)
                            
                 # Last the input layer
-                for i8 in range(len(self.input_neurons)):
-                    error = self.input_neurons[i8].train_adjust_synaptic_weights(out_minus_target)              
+                #for i8 in range(0, len(self.input_neurons)):
+                 #   error = self.input_neurons[i8].train_adjust_synaptic_weights(out_minus_target)              
              
             #break
             
             
     def think(self, inputs):
+        # each input paramter will have its own neuron
+        number_of_inputs = len(inputs)
+        #print("Number of inputs: ", number_of_inputs)
+        for i1 in range(0, number_of_inputs):
+            in_value = inputs[i1]
+            #print("Input value: ", in_value)
+        
+            self.input_neurons[i1].think_brain(in_value)
+        
+        # provide the output of the previous layer to the next layer
+        # There is only one hidden layer so keep this simple
+        input_next_layer = []
+        input_next_layer.append(987654)
+        for i2 in range(0, len(self.input_neurons)):
+            input_next_layer.extend(self.input_neurons[i2].normalized_neuron_value[0])
+#                    input_next_layer.extend(self.hidden_neurons[i2].normalized_neuron_value)
+            
+        input_next_layer = input_next_layer[1:]
+        #print("Variables for the next hidden layer: ",  input_next_layer)
+        #break
     
+        for i3 in range(0, len(self.hidden_neurons)):
+            self.hidden_neurons[i3].think_brain(input_next_layer)
+        #break
+            
+        # provide the output of the previous layer to the next layer
+        # There is only one hidden layer so keep this simple
+        input_next_layer = []
+        input_next_layer.append(987654)
+        for i4 in range(0, len(self.hidden_neurons)):
+            input_next_layer.extend(self.hidden_neurons[i4].normalized_neuron_value)
+            
+        input_next_layer = input_next_layer[1:]
+        #print("Variables for the next output layer: ",  input_next_layer)
+        #break
+        
+        # simple for nw, there is only on output neuron
+        output_neuron_value = 0
+        for i5 in range(len(self.output_neurons)):
+            output_neuron_value = self.output_neurons[i5].think_brain(input_next_layer)
+            
+        # just one output neuron, just return one value
+        return output_neuron_value
+        
+                    
 
 class Neuron():
     # give each Neuron a unique number
@@ -157,10 +165,18 @@ class Neuron():
         # We model a single neuron, with 3 input connections and 1 output connection.
         # We assign random weights to a 3 x 1 matrix, with values in the range -1 to 1
         # and mean 0.
-        self.synaptic_weights = 2 * random.random((number_of_inputs, 1)) - 1
+        self.synaptic_weights = []
+        if number_of_inputs is 1:
+            self.synaptic_weights.append([1.0])
+        else:
+            self.synaptic_weights = 2 * random.random((number_of_inputs, 1)) - 1
+            
         self.internal_neuron_value = 0
         self.normalized_neuron_value = 0
         self.training_rate = 0.5
+        self.out_minus_target = 0
+        self.derived_normalized_vs_value = 0
+
 
     def __repr__(self):
         return "The neuron layer #: " + str(self.neuron_layer_number) + \
@@ -197,33 +213,33 @@ class Neuron():
     def think_brain(self, in_value):
         self.training_set_inputs = in_value
         # Pass the training set through our neural network (a single neuron).
-        dot_product = dot(in_value, self.synaptic_weights)
-        self.internal_neuron_value = dot_product
-        #print("Dot product:", linesep, dot_product, linesep)
+        self.internal_neuron_value = dot(in_value, self.synaptic_weights)
+        #print("Dot product:", linesep, self.internal_neuron_value, linesep)
         
-        after_sigmoid = self.__sigmoid(dot_product)
-        #print("After sigmoid:", linesep, after_sigmoid, linesep)
-        
-        self.normalized_neuron_value = after_sigmoid
-        return self.normalized_neuron_value 
+        self.normalized_neuron_value = self.__sigmoid(self.internal_neuron_value)
+        #print("After sigmoid:", linesep, self.normalized_neuron_value, linesep)
+       
+        return self.normalized_neuron_value
+
 
     # We train the neural network through a process of trial and error.
     # Adjusting the synaptic weights each time.
-    def train_adjust_synaptic_weights(self, expected_output):
+    def train_adjust_synaptic_weights_for_output_neuron(self, output_neuron_value, ):
         # https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
         # https://en.wikipedia.org/wiki/Logistic_function#Derivative
         #print("Adjust parameters - input values", linesep,  self.training_set_inputs)
         #print(linesep, "Adjust parameters - weights", linesep,  self.synaptic_weights)
         
-        out_minus_target = -1 * ( expected_output -  self.normalized_neuron_value)
-        derived_normalized_vs_value = self.normalized_neuron_value * ( 1 - self.normalized_neuron_value)
+        
+        self.out_minus_target = -1 * (output_neuron_value - self.normalized_neuron_value)
+        self.derived_normalized_vs_value = self.normalized_neuron_value * ( 1 - self.normalized_neuron_value)
         
         for i in range(0, len(self.synaptic_weights)):
             weight = self.synaptic_weights[i]
             derived_value_vs_parameter = self.training_set_inputs[i]
             partial_derived_for_parameter = \
-                out_minus_target * \
-                derived_normalized_vs_value * \
+                self.out_minus_target * \
+                self.derived_normalized_vs_value * \
                 derived_value_vs_parameter
                 
             
@@ -232,6 +248,35 @@ class Neuron():
         #print("Adjust parameters - input values", linesep,  self.training_set_inputs)         
         #print("Adjust parameters - new weights", linesep,  self.synaptic_weights) 
     
+
+   # We train the neural network through a process of trial and error.
+    # Adjusting the synaptic weights each time.
+    def train_adjust_synaptic_weights_for_hidden_neuron(self, needed_value):
+        # https://mattmazur.com/2015/03/17/a-step-by-step-backpropagation-example/
+        # https://en.wikipedia.org/wiki/Logistic_function#Derivative
+        #print("Adjust parameters - input values", linesep,  self.training_set_inputs)
+        #print(linesep, "Adjust parameters - weights", linesep,  self.synaptic_weights)
+        
+        self.out_minus_target = needed_value
+        self.derived_normalized_vs_value = self.normalized_neuron_value * ( 1 - self.normalized_neuron_value)
+        
+        for i in range(0, len(self.synaptic_weights)):
+            weight = self.synaptic_weights[i]
+            derived_value_vs_parameter = self.training_set_inputs[i]
+            partial_derived_for_parameter = \
+                self.out_minus_target * weight * \
+                self.derived_normalized_vs_value * \
+                derived_value_vs_parameter
+                
+            
+            
+            self.synaptic_weights[i] = weight - self.training_rate * partial_derived_for_parameter
+        #print("Adjust parameters - input values", linesep,  self.training_set_inputs)         
+        #print("Adjust parameters - new weights", linesep,  self.synaptic_weights) 
+    
+
+
+
 
     # The neural network thinks.
     def think(self, inputs):
