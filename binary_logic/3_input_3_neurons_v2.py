1aa244557e4ac56b1bf70237dd25edd759ae4f7a
wgruen <wolf128git@gmail.com>
2019-06-05 21:59:19 -0400

    change expedcted output to 0.01 and 0.99

--- binary_logic/3_input_3_neurons_v2.py
+++ binary_logic/3_input_3_neurons_v2.py
@@ -18,6 +18,7 @@ from os import linesep
 import os
 import sys
 import pprint
+import pickle
 
 
 class NeuralNetwork():
@@ -466,10 +467,11 @@ if __name__ == "__main__":
                                  [1, 0, 1],
                                  [0, 1, 1]])
     
-    training_set_outputs = array([[0,
-                                   1,
-                                   1,
-                                   0]]).T
+   
+    training_set_outputs = array([[0.01,
+                                   0.99,
+                                   0.01,
+                                   0.99]]).T
     
     
     #Intialise a single neuron neural network.
@@ -486,7 +488,7 @@ if __name__ == "__main__":
 
     # Train the neural network using a training set.
     # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+    neural_network.train(training_set_inputs, training_set_outputs, 1000)
     
     #sys.stdout = sys.__stdout__
     
@@ -494,6 +496,32 @@ if __name__ == "__main__":
     print("New synaptic weights after training:", linesep)
     print(neural_network.show_synaptic_weights(), linesep)
     print("All matrixes: ", neural_network.show_matrices())
+    
+    
+    with open("neural_net_trained", 'bw+') as outfile:
+        pickle.dump(neural_network.training_rate, outfile)
+        pickle.dump(neural_network.neuron_weigths_holder, outfile)
+        
+        
+        a_len = len(neural_network.neuron_weigths_holder)
+        for i in range(0, a_len):
+            a2_len = len(neural_network.neuron_weigths_holder[i])
+            a = np.ndarray(a2_len, buffer=neural_network.neuron_weigths_holder[i])
+            print(a)
+            #a.dump(outfile)
+            #pickle.dump(a.dump, outfile)
+            pickle.dump(neural_network.neuron_weigths_holder[i], outfile)
+            
+        
+    # finish the write and read later
+    #read the pickle file
+    pickle_in = open("neural_net_trained","rb")
+    print("Read pickle data")
+    pickle_data = pickle.load(pickle_in)
+    print(pickle_data)
+    
+        
+    
 
     # Test the neural network with a new situation.
     test_data = array([[0, 0, 0], 
