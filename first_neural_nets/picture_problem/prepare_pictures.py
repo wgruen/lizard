42719aed567d33f732352dd8ec28a212bcea77e5
Wolfgang Gruen <wolfgit@gmail.com>
2020-02-12 20:26:00 -0500

    playing with keras

--- first_neural_nets/picture_problem/prepare_pictures.py
+++ first_neural_nets/picture_problem/prepare_pictures.py
@@ -1,106 +1,170 @@
 #!/usr/bin/env python3
 
+
+# https://machinelearningmastery.com/how-to-develop-a-cnn-from-scratch-for-cifar-10-photo-classification/
+
 import sys
-import os
-import inspect
-import importlib
-from numpy import exp, array, random, dot, round
-import numpy as np
-from os import linesep
-
-
-# use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"/../nr_lib")))
-if cmd_subfolder not in sys.path:
-    sys.path.append(cmd_subfolder)
-print(sys.path)
-
-
-from nr_1_hidden_layer import NeuralNetwork
+from matplotlib import pyplot
 
 
 
-if __name__ == "__main__":
+# example of loading the cifar10 dataset
+from keras.datasets import cifar10
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
 
-    # The training set. We have 5 examples, each consisting of 3 input values
-    # and 1 expectd output value.
-    training_set_inputs = array([[0, 0, 1], 
-                                 [1, 1, 0],
-                                 [1, 0, 1],
-                                 [0, 1, 1]])
+
+# load train and test dataset
+def load_dataset():
+    # load dataset
+    (trainX, trainY), (testX, testY) = cifar10.load_data()
     
+    # convert into form that can be used to ML algorithm
+    # one hot encode target values
+    # to_categorical returns a binary matrix
+    trainY = to_categorical(trainY)
+    testY = to_categorical(testY)
+    return trainX, trainY, testX, testY
+
+
+
+
+def print_dataset(trainX, trainY, testX, testY):
+    # summarize loaded dataset
+    print('Train: X=%s, y=%s' % (trainX.shape, trainY.shape))
+    print('Test: X=%s, y=%s' % (testX.shape, testY.shape))
+    # plot first few images
+    for i in range(1):
+        # define subplot
+        pyplot.subplot(330 + 1 + i)
+        
+        # plot raw pixel data
+        print("\npicture shape:", trainX[i].shape)
+        print("picture 32x32 pixels - row:", len(trainX[i]))
+        print("picture 32x32 pixels - column  :", len(trainX[i][1]))
+        print("picture one pixel RGB  :", len(trainX[i][1][1]))
+        print("picture data:\n", trainX[i])
+        
+        pyplot.imshow(trainX[i])
+    
+    
+    # show the figure
+    pyplot.show()
+    
+    
+# The integers are in the range of 0 to 255
+# Here we normalize the picture values.
+# Rescale them to the range [0,1]
+def prepare_data(data):
+    # convert from integers to floats
+    data_norm = data.astype('float32')
+    
+    # normalize to range 0-1
+    data_norm = data_norm / 255.0
+    
+    # return normalized images
+    return data_norm
+    
+
+# Here we build the layers of the CNN (Convolutional Neural Networks) model
+def define_model():
+	model = Sequential()
+    
+    # https://machinelearningmastery.com/object-recognition-convolutional-neural-networks-keras-deep-learning-library/
    
-    training_set_outputs = array([[0.01,
-                                   0.99,
-                                   0.01,
-                                   0.99]]).T
+    # input shape
+    ## how many elements and array or tensor has in each dimension
+    ## three dimensions
+    ## 32 elements in the first dimension
+    ## 32 elements in the second dimension
+    ## 3  elements in the third dimension
+    ### The pictures (data) has 32 times 32 pixels
+    ### Each pixel has a value for its Red, Green and Blue 
     
+    # Conv2D
+    # https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2D
+    # Total number of inputs is 32x32 = 1024 and three values (RGB) = 3072
+    # activation relu = y = max(0, x)
+    # VG based model
+    # https://medium.com/datadriveninvestor/cnn-architecture-series-vgg-16-with-implementation-part-i-bca79e7db415
     
-    #Intialise a single neuron neural network.
-    #number_of_input_neurons, number_of_hidden_neurons, number_of_hidden_layers, number_of_outputs)
-    # cueently only on hidden layer is supported
-    neural_network = NeuralNetwork(3, 4, 1, 1)
+	model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
+	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 
+	model.add(MaxPooling2D(pool_size=(2, 2)))
     
+    
+	model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+    
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+	model.add(MaxPooling2D((2, 2)))
+    
+    # Flatten the input
+	model.add(Flatten())
+    
+    # First hidden network layer with 128 Neurons
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+    
+    # The output layer with 10 Neurons - output layer using softmax
+	model.add(Dense(10, activation='softmax'))
+    
+    
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+	return model
+
+
+
+'''
+The parameters are self explaining
+
+'''
+def main(argv):
+    # load and prepare the dataset
+    trainX, trainY, testX, testY = load_dataset()
+    print_dataset(trainX, trainY, testX, testY)
+    
+    ## TRAIN / FIT the model
+    # prepare pixel data
+    trainX = prepare_data(trainX)
+    
+    # define and use a model
+    model = define_model()
+    print("model weights:\n", model.get_weights())
+    print("model summary:\n", model.summary())
+    #print("model:\n", model.get_weights())
+    
+    # fit the model
+    model.fit(trainX, trainY,\
+        epochs=1,\
+        #100,\
+        batch_size=64,\
+        validation_data=(testX, testY),\
+        verbose=1)
+    
+    print(model.get_config())
+    
+    
+    ## EVALUATE the previously trained model
+    testX = prepare_data(testX)
+    #_, acc = model.evaluate(testX, testY, verbose=0)
   
-    print("Random starting synaptic weights:", linesep)
-    print(neural_network.show_synaptic_weights(), linesep)
+if __name__ == '__main__':
+    main(sys.argv[1:])
     
     
-    #sys.stdout = open(os.devnull, 'w')
-
-    # Train the neural network using a training set.
-    # Do it 10,000 times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, 1000)
-    #sys.stdout = sys.__stdout_
-    
-
-    print("New synaptic weights after training:", linesep)
-    print(neural_network.show_synaptic_weights(), linesep)
-    print("All matrixes: ", neural_network.show_matrices())
-    
-    '''
-    with open("neural_net_trained", 'bw+') as outfile:
-        pickle.dump(neural_network.training_rate, outfile)
-        pickle.dump(neural_network.neuron_weigths_holder, outfile)
-        
-        
-        a_len = len(neural_network.neuron_weigths_holder)
-        for i in range(0, a_len):
-            a2_len = len(neural_network.neuron_weigths_holder[i])
-            a = np.ndarray(a2_len, buffer=neural_network.neuron_weigths_holder[i])
-            print(a)
-            #a.dump(outfile)
-            #pickle.dump(a.dump, outfile)
-            pickle.dump(neural_network.neuron_weigths_holder[i], outfile)
-            
-        
-    # finish the write and read later
-    #read the pickle file
-    pickle_in = open("neural_net_trained","rb")
-    print("Read pickle data")
-    pickle_data = pickle.load(pickle_in)
-    print(pickle_data)
-    
- '''  
-    
-
-    # Test the neural network with a new situation.
-    test_data = array([[0, 0, 0], 
-                       [0, 0, 1],
-                       [0, 1, 0],
-                       [0, 1, 1],
-                       [1, 0, 0],
-                       [1, 0, 1],
-                       [1, 1, 0],
-                       [1, 1, 1]])
-    
-    for data in test_data:
-        print("Considering new situation", data, "-> ",  neural_network.think(data))
     
 
 
-    
-    
-    
-    
+
+
+
